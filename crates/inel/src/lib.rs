44afafa3368219ceb3694899c89199b067737dbb
mihneabuz <mihneabuzatu88@gmail.com>
2024-10-19 13:26:11 +0300

    expose reactor internals behind feature

--- crates/inel/src/lib.rs
+++ crates/inel/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{cell::RefCell, future::Future};
+use core::{cell::RefCell, future::Future};
 
 use inel_executor::{Executor, JoinHandle};
 use inel_reactor::Ring;
@@ -26,6 +26,7 @@ impl inel_interface::Reactor for GlobalReactor {
 
 pub use inel_macro::main;
 
+#[inline]
 pub fn spawn<F>(future: F) -> JoinHandle<F::Output>
 where
     F: Future + 'static,
@@ -33,6 +34,7 @@ where
     EXECUTOR.with_borrow(|exe| exe.spawn(future))
 }
 
+#[inline]
 pub fn block_on<F>(future: F) -> F::Output
 where
     F: Future + 'static,
@@ -40,6 +42,26 @@ where
     EXECUTOR.with_borrow(|exe| exe.block_on_with_reactor(GlobalReactor, future))
 }
 
+#[inline]
 pub fn run() {
-    EXECUTOR.with_borrow(|exe| exe.run_with_reactor(GlobalReactor))
+    EXECUTOR.with_borrow(|exe| exe.run_with_reactor(GlobalReactor));
+}
+
+#[cfg(feature = "sys")]
+pub mod sys {
+    use super::GlobalReactor;
+
+    pub use inel_reactor::{buffer::*, op, Submission};
+
+    #[allow(private_interfaces)]
+    #[inline]
+    pub const fn reactor() -> GlobalReactor {
+        GlobalReactor
+    }
+
+    #[allow(private_interfaces)]
+    #[inline]
+    pub fn wrap<O: op::Op>(op: O) -> Submission<O, GlobalReactor> {
+        op.run_on(reactor())
+    }
 }
