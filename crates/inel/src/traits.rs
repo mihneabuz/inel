9358c9e8e975680a094ceffe4282aa4bd4da1285
mihneabuz <mihneabuzatu88@gmail.com>
2024-11-04 18:33:47 +0200

    inel: added read/write at methods

--- crates/inel/src/traits.rs
+++ crates/inel/src/traits.rs
@@ -9,20 +9,41 @@ pub(crate) trait WriteSource: AsRawFd {}
 
 #[allow(async_fn_in_trait)]
 pub trait InelRead {
-    async fn read_owned<B: StableMutBuffer>(&mut self, buf: B) -> (B, Result<usize>);
+    async fn read_owned<B: StableMutBuffer>(&mut self, buffer: B) -> (B, Result<usize>);
+    async fn read_owned_at<B: StableMutBuffer>(
+        &mut self,
+        offset: u64,
+        buffer: B,
+    ) -> (B, Result<usize>);
 }
 
 #[allow(async_fn_in_trait)]
 pub trait InelWrite {
-    async fn write_owned<B: StableBuffer>(&mut self, buf: B) -> (B, Result<usize>);
+    async fn write_owned<B: StableBuffer>(&mut self, buffer: B) -> (B, Result<usize>);
+    async fn write_owned_at<B: StableBuffer>(
+        &mut self,
+        offset: u64,
+        buffer: B,
+    ) -> (B, Result<usize>);
 }
 
 impl<T> InelRead for T
 where
     T: ReadSource,
 {
-    async fn read_owned<B: StableMutBuffer>(&mut self, buf: B) -> (B, Result<usize>) {
-        op::Read::new(self.as_raw_fd(), buf)
+    async fn read_owned<B: StableMutBuffer>(&mut self, buffer: B) -> (B, Result<usize>) {
+        op::Read::new(self.as_raw_fd(), buffer)
+            .run_on(GlobalReactor)
+            .await
+    }
+
+    async fn read_owned_at<B: StableMutBuffer>(
+        &mut self,
+        offset: u64,
+        buffer: B,
+    ) -> (B, Result<usize>) {
+        op::Read::new(self.as_raw_fd(), buffer)
+            .offset(offset)
             .run_on(GlobalReactor)
             .await
     }
@@ -32,8 +53,19 @@ impl<T> InelWrite for T
 where
     T: WriteSource,
 {
-    async fn write_owned<B: StableBuffer>(&mut self, buf: B) -> (B, Result<usize>) {
-        op::Write::new(self.as_raw_fd(), buf)
+    async fn write_owned<B: StableBuffer>(&mut self, buffer: B) -> (B, Result<usize>) {
+        op::Write::new(self.as_raw_fd(), buffer)
+            .run_on(GlobalReactor)
+            .await
+    }
+
+    async fn write_owned_at<B: StableBuffer>(
+        &mut self,
+        offset: u64,
+        buffer: B,
+    ) -> (B, Result<usize>) {
+        op::Write::new(self.as_raw_fd(), buffer)
+            .offset(offset)
             .run_on(GlobalReactor)
             .await
     }
