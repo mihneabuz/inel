19788e56770385b0d07c194a3d8fac2c4fc5c6db
mihneabuz <mihneabuzatu88@gmail.com>
2024-11-10 13:26:20 +0200

    inel-reactor: added tests

--- crates/inel-reactor/tests/main/read.rs
+++ crates/inel-reactor/tests/main/read.rs
@@ -171,7 +171,7 @@ fn multi() {
 fn error() {
     let (reactor, notifier) = runtime();
 
-    let mut read = op::Read::new(RawFd::from(192), Box::new([0; 128])).run_on(reactor.clone());
+    let mut read = op::Read::new(RawFd::from(9999), Box::new([0; 128])).run_on(reactor.clone());
     let mut fut = pin!(&mut read);
 
     assert!(poll!(fut, notifier).is_pending());
@@ -444,5 +444,217 @@ mod vectored {
         assert!(fut1.is_terminated());
 
         assert!(reactor.is_done());
+    }
+}
+
+mod fixed {
+    use inel_reactor::buffer::Fixed;
+
+    use super::*;
+
+    #[test]
+    fn single() {
+        let (reactor, notifier) = runtime();
+        let file = TempFile::with_content(&MESSAGE);
+
+        let buf = Fixed::register(Box::new([0; 1024]), reactor.clone()).unwrap();
+        let mut read = op::ReadFixed::new(file.fd(), buf).run_on(reactor.clone());
+        let mut fut = pin!(&mut read);
+
+        assert!(poll!(fut, notifier).is_pending());
+        assert_eq!(reactor.active(), 1);
+
+        reactor.wait();
+
+        assert_eq!(notifier.try_recv(), Some(()));
+
+        let Poll::Ready((buf, res)) = poll!(fut, notifier) else {
+            panic!("poll not ready");
+        };
+        let read = res.expect("read failed");
+
+        assert_eq!(read, 1024);
+        assert_eq!(&buf.inner()[..read], &MESSAGE.as_bytes()[..read]);
+
+        assert!(fut.is_terminated());
+        assert!(reactor.is_done());
+    }
+
+    #[test]
+    fn view() {
+        let (reactor, notifier) = runtime();
+        let file = TempFile::with_content(&MESSAGE);
+
+        let buf = Fixed::register(Box::new([0; 1024]), reactor.clone()).unwrap();
+        let mut read = op::ReadFixed::new(file.fd(), buf.view(64..512)).run_on(reactor.clone());
+        let mut fut = pin!(&mut read);
+
+        assert!(poll!(fut, notifier).is_pending());
+        assert_eq!(reactor.active(), 1);
+
+        reactor.wait();
+
+        assert_eq!(notifier.try_recv(), Some(()));
+
+        let Poll::Ready((view, res)) = poll!(fut, notifier) else {
+            panic!("poll not ready");
+        };
+        let read = res.expect("read failed");
+
+        assert_eq!(&view.as_slice()[..read], &MESSAGE.as_bytes()[..read]);
+
+        let buf = view.unview();
+        assert_eq!(&buf.inner()[0..64], &[0; 64]);
+        assert_eq!(&buf.inner()[512..], &[0; 512]);
+
+        assert!(fut.is_terminated());
+        assert!(reactor.is_done());
+    }
+
+    #[test]
+    #[test_repeat(100)]
+    fn multi() {
+        let (reactor, notifier) = runtime();
+
+        let file1 = TempFile::with_content(&MESSAGE);
+        let file2 = TempFile::with_content(&MESSAGE);
+        let file3 = TempFile::with_content(&MESSAGE);
+
+        const READ_LEN1: usize = 128;
+        const READ_LEN2: usize = 256;
+        const READ_LEN3: usize = 512;
+
+        let buf1 = Fixed::register(Box::new([0; READ_LEN1]), reactor.clone()).unwrap();
+        let buf2 = Fixed::register(Box::new([0; READ_LEN2]), reactor.clone()).unwrap();
+        let buf3 = Fixed::register(Vec::from([0; READ_LEN3]), reactor.clone()).unwrap();
+
+        let mut read1 = op::ReadFixed::new(file1.fd(), buf1).run_on(reactor.clone());
+        let mut read2 = op::ReadFixed::new(file2.fd(), buf2).run_on(reactor.clone());
+        let mut read3 = op::ReadFixed::new(file3.fd(), buf3).run_on(reactor.clone());
+
+        let mut fut1 = pin!(&mut read1);
+        let mut fut2 = pin!(&mut read2);
+        let mut fut3 = pin!(&mut read3);
+
+        assert!(poll!(fut1, notifier).is_pending());
+        assert!(poll!(fut2, notifier).is_pending());
+        assert!(poll!(fut3, notifier).is_pending());
+
+        assert_eq!(reactor.active(), 3);
+
+        reactor.wait();
+
+        assert_eq!(notifier.try_recv(), Some(()));
+        assert!(reactor.active() < 3);
+
+        reactor.wait();
+        reactor.wait();
+
+        assert_eq!(notifier.try_recv(), Some(()));
+        assert_eq!(notifier.try_recv(), Some(()));
+        assert_eq!(reactor.active(), 0);
+
+        let Poll::Ready((buf1, res1)) = poll!(fut1, notifier) else {
+            panic!("poll 1 not ready");
+        };
+        let read1 = res1.expect("read 1 failed");
+
+        let Poll::Ready((buf2, res2)) = poll!(fut2, notifier) else {
+            panic!("poll 2 not ready");
+        };
+        let read2 = res2.expect("read 2 failed");
+
+        let Poll::Ready((buf3, res3)) = poll!(fut3, notifier) else {
+            panic!("poll 3 not ready");
+        };
+        let read3 = res3.expect("read 3 failed");
+
+        assert_eq!(read1, READ_LEN1);
+        assert_eq!(&buf1.inner()[..read1], &MESSAGE.as_bytes()[..read1]);
+
+        assert_eq!(read2, READ_LEN2);
+        assert_eq!(&buf2.inner()[..read2], &MESSAGE.as_bytes()[..read2]);
+
+        assert_eq!(read3, READ_LEN3);
+        assert_eq!(&buf3.inner()[..read3], &MESSAGE.as_bytes()[..read3]);
+
+        assert!(fut1.is_terminated());
+        assert!(fut2.is_terminated());
+        assert!(fut3.is_terminated());
+
+        assert!(reactor.is_done());
+    }
+
+    #[test]
+    fn error() {
+        let (reactor, notifier) = runtime();
+
+        let buf = Fixed::register(Box::new([0; 128]), reactor.clone()).unwrap();
+        let mut read = op::ReadFixed::new(RawFd::from(9999), buf).run_on(reactor.clone());
+        let mut fut = pin!(&mut read);
+
+        assert!(poll!(fut, notifier).is_pending());
+        assert_eq!(reactor.active(), 1);
+
+        reactor.wait();
+
+        assert_eq!(notifier.try_recv(), Some(()));
+
+        let Poll::Ready((buf, res)) = poll!(fut, notifier) else {
+            panic!("poll not ready");
+        };
+
+        assert_eq!(buf.into_inner(), Box::new([0; 128]));
+        res.expect_err("read didn't fail");
+    }
+
+    #[test]
+    #[test_repeat(100)]
+    fn cancel() {
+        let (reactor, notifier) = runtime();
+        let file1 = TempFile::with_content(&MESSAGE);
+        let file2 = TempFile::with_content(&MESSAGE);
+        let file3 = TempFile::with_content(&MESSAGE);
+
+        const READ_LEN1: usize = 1024;
+        const READ_LEN2: usize = 2048;
+        const READ_LEN3: usize = 2048;
+
+        let buf1 = Fixed::register(Box::new([0; READ_LEN1]), reactor.clone()).unwrap();
+        let buf2 = Fixed::register(Box::new([0; READ_LEN2]), reactor.clone()).unwrap();
+        let buf3 = Fixed::register(Vec::from([0; READ_LEN3]), reactor.clone()).unwrap();
+
+        let mut read1 = op::ReadFixed::new(file1.fd(), buf1).run_on(reactor.clone());
+        let mut read2 = op::ReadFixed::new(file2.fd(), buf2)
+            .offset(128)
+            .run_on(reactor.clone());
+        let mut read3 = op::ReadFixed::new(file3.fd(), buf3.view(128..)).run_on(reactor.clone());
+
+        let mut fut1 = pin!(&mut read1);
+        let mut fut2 = pin!(&mut read2);
+        let mut fut3 = pin!(&mut read3);
+        let mut nop = pin!(&mut op::Nop.run_on(reactor.clone()));
+
+        assert!(poll!(fut1, notifier).is_pending());
+        assert!(poll!(fut2, notifier).is_pending());
+        assert!(poll!(fut3, notifier).is_pending());
+        assert!(poll!(nop, notifier).is_pending());
+
+        assert_eq!(reactor.active(), 4);
+
+        reactor.wait();
+
+        assert!(poll!(nop, notifier).is_ready());
+
+        drop(read1);
+        drop(read2);
+        drop(read3);
+
+        let mut i = 0;
+        while !reactor.is_done() {
+            reactor.wait();
+            i += 1;
+            assert!(i < 5);
+        }
     }
 }
