647a697dbca5add9738e70458b27dae0069ade95
mihneabuz <mihneabuzatu88@gmail.com>
2024-11-21 23:38:16 +0200

    inel: change Fixed to not expose private GlobalReactor

--- crates/inel/src/buffer.rs
+++ crates/inel/src/buffer.rs
@@ -3,9 +3,7 @@ use std::{io::Result, ops::RangeBounds};
 use crate::GlobalReactor;
 
 pub use inel_reactor::buffer::{FixedBuffer, StableBuffer, View};
-
-#[allow(private_interfaces)]
-pub type Fixed<B> = inel_reactor::buffer::Fixed<B, GlobalReactor>;
+use inel_reactor::{BufferKey, Cancellation};
 
 pub trait StableBufferExt: StableBuffer {
     fn view<R>(self, range: R) -> View<Self, R>
@@ -13,7 +11,6 @@ pub trait StableBufferExt: StableBuffer {
         R: RangeBounds<usize>,
         Self: Sized;
 
-    #[allow(private_interfaces)]
     fn fix(self) -> Result<Fixed<Self>>
     where
         Self: Sized;
@@ -27,11 +24,61 @@ where
         View::new(self, range)
     }
 
-    #[allow(private_interfaces)]
     fn fix(self) -> Result<Fixed<Self>>
     where
         Self: Sized,
     {
-        Fixed::register(self, GlobalReactor)
+        Fixed::register(self)
+    }
+}
+
+pub struct Fixed<B: StableBuffer>(inel_reactor::buffer::Fixed<B, GlobalReactor>);
+
+impl<B: StableBuffer> Fixed<B> {
+    pub fn register(buf: B) -> Result<Self> {
+        let raw = inel_reactor::buffer::Fixed::register(buf, GlobalReactor)?;
+        Ok(Self(raw))
+    }
+
+    pub fn inner(&self) -> &B {
+        self.0.inner()
+    }
+
+    pub fn inner_mut(&mut self) -> &mut B {
+        self.0.inner_mut()
+    }
+
+    pub fn into_inner(self) -> B {
+        self.0.into_inner()
+    }
+}
+
+impl<B: StableBuffer> From<Fixed<B>> for Cancellation {
+    fn from(value: Fixed<B>) -> Self {
+        value.0.into_inner().into()
+    }
+}
+
+impl<B: StableBuffer> StableBuffer for Fixed<B> {
+    fn stable_ptr(&self) -> *const u8 {
+        self.0.stable_ptr()
+    }
+
+    fn stable_mut_ptr(&mut self) -> *mut u8 {
+        self.0.stable_mut_ptr()
+    }
+
+    fn size(&self) -> usize {
+        self.0.size()
+    }
+
+    fn capacity(&self) -> usize {
+        self.0.capacity()
+    }
+}
+
+impl<B: StableBuffer> FixedBuffer for Fixed<B> {
+    fn key(&self) -> &BufferKey {
+        self.0.key()
     }
 }
