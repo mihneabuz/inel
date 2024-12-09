fc95bf84a98f51069d8ca19acffebec62ff39c07
mihneabuz <mihneabuzatu88@gmail.com>
2024-12-10 01:04:48 +0200

    inel: added Instant future

--- crates/inel/src/time.rs
+++ crates/inel/src/time.rs
@@ -42,3 +42,39 @@ impl FusedFuture for Timeout {
         self.sub.is_terminated()
     }
 }
+
+pub fn instant() -> Instant {
+    Instant::default()
+}
+
+pub struct Instant {
+    sub: Submission<op::Nop, GlobalReactor>,
+}
+
+impl Instant {
+    pub fn new() -> Self {
+        Self {
+            sub: op::Nop.run_on(GlobalReactor),
+        }
+    }
+}
+
+impl Default for Instant {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Future for Instant {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        Pin::into_inner(self).sub.poll_unpin(cx)
+    }
+}
+
+impl FusedFuture for Instant {
+    fn is_terminated(&self) -> bool {
+        self.sub.is_terminated()
+    }
+}
