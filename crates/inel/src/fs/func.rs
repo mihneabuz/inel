492a21fc56f3f0191b1f9be9bb382002eafe0b99
mihneabuz <mihneabuzatu88@gmail.com>
2025-03-29 16:21:55 +0200

    inel: added some more chained ops funcs

---
+++ crates/inel/src/fs/func.rs
@@ -0,0 +1,154 @@
+use std::{
+    future::Future,
+    io::Result,
+    path::Path,
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+use futures::join;
+use inel_interface::Reactor;
+use inel_reactor::{
+    buffer::StableBuffer,
+    op::{self, OpExt},
+    FileSlotKey,
+};
+
+use crate::GlobalReactor;
+
+macro_rules! btry {
+    ($buf:expr, $res:expr) => {{
+        match $res {
+            Ok(x) => ($buf, x),
+            Err(e) => {
+                return ($buf, Err(e));
+            }
+        }
+    }};
+}
+
+pub async fn exists<P>(path: P) -> bool
+where
+    P: AsRef<Path>,
+{
+    with_slot(async |slot| {
+        let open = op::OpenAt::new(path, libc::O_WRONLY)
+            .fixed(slot)
+            .chain()
+            .run_on(GlobalReactor);
+
+        let close = op::Close::new(slot).run_on(GlobalReactor);
+
+        let (open, _) = join!(open, close);
+
+        open.map(|_| true).unwrap_or(false)
+    })
+    .await
+}
+
+pub async fn write<P, C>(path: P, contents: C) -> (C, Result<()>)
+where
+    P: AsRef<Path>,
+    C: StableBuffer,
+{
+    with_slot(async |slot| {
+        let open = op::OpenAt::new(path, libc::O_WRONLY | libc::O_CREAT | libc::O_TRUNC)
+            .mode(0o666)
+            .fixed(slot)
+            .chain()
+            .run_on(GlobalReactor);
+        let write = op::Write::new(slot, contents).chain().run_on(GlobalReactor);
+        let close = op::Close::new(slot).run_on(GlobalReactor);
+
+        let (open, write, _) = join!(open, write, close);
+        let (buf, write) = write;
+
+        let (buf, _) = btry!(buf, open);
+        let (buf, wrote) = btry!(buf, write);
+
+        assert_eq!(buf.size(), wrote);
+
+        (buf, Ok(()))
+    })
+    .await
+}
+
+pub async fn read<P, B>(path: P, buffer: B) -> (B, Result<usize>)
+where
+    P: AsRef<Path>,
+    B: StableBuffer,
+{
+    with_slot(async |slot| {
+        let open = op::OpenAt::new(path, libc::O_RDWR)
+            .fixed(slot)
+            .chain()
+            .run_on(GlobalReactor);
+        let read = op::Read::new(slot, buffer).chain().run_on(GlobalReactor);
+        let close = op::Close::new(slot).run_on(GlobalReactor);
+
+        let (open, read, _) = join!(open, read, close);
+        let (buf, read) = read;
+
+        let (buf, _) = btry!(buf, open);
+        let (buf, read) = btry!(buf, read);
+
+        (buf, Ok(read))
+    })
+    .await
+}
+
+async fn with_slot<F, H, T>(f: F) -> T
+where
+    F: FnOnce(FileSlotKey) -> H,
+    H: Future<Output = T>,
+{
+    WithSlot::new(f).unwrap().await
+}
+
+pin_project_lite::pin_project! {
+    struct WithSlot<F, T>
+    where
+        F: Future<Output = T>,
+    {
+        #[pin]
+        future: F,
+        slot: FileSlotKey,
+    }
+
+    impl<F, T> PinnedDrop for WithSlot<F, T>
+    where
+        F: Future<Output = T>,
+    {
+        fn drop(this: Pin<&mut Self>) {
+            let slot = *this.project().slot;
+            GlobalReactor
+                .with(|react| react.release_file_slot(slot))
+                .unwrap();
+        }
+    }
+}
+
+impl<F, T> WithSlot<F, T>
+where
+    F: Future<Output = T>,
+{
+    pub fn new<H>(f: H) -> Result<Self>
+    where
+        H: FnOnce(FileSlotKey) -> F,
+    {
+        let slot = GlobalReactor.with(|react| react.get_file_slot()).unwrap()?;
+        let future = f(slot);
+        Ok(Self { future, slot })
+    }
+}
+
+impl<F, T> Future for WithSlot<F, T>
+where
+    F: Future<Output = T>,
+{
+    type Output = T;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        self.project().future.poll(cx)
+    }
+}
--- readme.md
+++ readme.md
@@ -5,5 +5,4 @@ Single-threaded Event Loop async runtime powered by io_uring. Linux chads only.
 ## WIP
 
 ### todo:
- - chained ops (eg. open + read + close)
  - buffer selection
